2b99542a381a935a550bad91f3652d197b5d7921
Jairosher <50308082+Jairosher@users.noreply.github.com>
2019-06-09 13:12:24 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports ={
+
+	"APP_TOKEN" :"EAAJctUvM30oBAJyYikpn50ghIbfpo4NI6IZBDXU2rSjh9k88prDxpuhYPD5eFDKrWJacrI0nZCSfF01d9UPcxhTZCotnkmh8HclDYMxjG3iyB2EpzcDi3P1fGtBtP43bcUVUZAiQ6YtaP2JUBIoak8LQiao84Q5EYOedkPAZAVwZDZD", // todo esto se obtiene cuando creas tu aplicacion en developers.facebook.com
+	"FACEBOOK_TOKEN": "cualquier_token_funciona",
+	"URI":"https://graph.facebook.com/v2.10/me/messages" //API
+	
+}
