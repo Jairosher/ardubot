cf5936b08ec9061e221aedf971478b5925a532c0
Jairosher <50308082+Jairosher@users.noreply.github.com>
2019-06-16 13:39:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -25,11 +25,11 @@ var app=express();
 app.use(bodyParser.json());
 
 
-//app.listen((process.env.PORT || 5000), () => console.log('El servidor webhook esta escuchando!'));
+app.listen((process.env.PORT || 3000), () => console.log('El servidor webhook esta escuchando!'));
 
-   app.listen(3000, function(){
-        console.log('El servidor puerto 5000');
-    });
+   //app.listen(3000, function(){
+     //   console.log('El servidor puerto 5000');
+    //});
 
 app.get('/', function(req,res){
 
